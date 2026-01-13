5a20d45504731b31884b5474e759f21e500734bd
vishaljha2121 <51633569+vishaljha2121@users.noreply.github.com>
2026-01-13 14:52:18 -0500

    Adding arena.c and arena.h files for arena memory allocation

---
+++ src/memory_mngmnt/arena.c
@@ -0,0 +1,224 @@
+//
+// Created by Vishal Jha on 13/01/26.
+//
+
+#include "arena.h"
+
+mem_arena* arena_create(u64 reserve_size, u64 commit_size) {
+    u32 pagesize = plat_get_pagesize();
+
+    reserve_size = ALIGN_UP_POW2(reserve_size, pagesize);
+    commit_size = ALIGN_UP_POW2(commit_size, pagesize);
+
+    mem_arena* arena = plat_mem_reserve(reserve_size);
+
+    if (!plat_mem_commit(arena, commit_size)) {
+        return NULL;
+    }
+
+    arena->reserve_size = reserve_size;
+    arena->commit_size = commit_size;
+    arena->pos = ARENA_BASE_POS;
+    arena->commit_pos = commit_size;
+
+    return arena;
+}
+
+void arena_destroy(mem_arena* arena) {
+    plat_mem_release(arena, arena->reserve_size);
+}
+
+void* arena_push(mem_arena* arena, u64 size, b32 non_zero) {
+    u64 pos_aligned = ALIGN_UP_POW2(arena->pos, ARENA_ALIGN);
+    u64 new_pos = pos_aligned + size;
+
+    if (new_pos > arena->reserve_size) { return NULL; }
+
+    if (new_pos > arena->commit_pos) {
+        u64 new_commit_pos = new_pos;
+        new_commit_pos += arena->commit_size - 1;
+        new_commit_pos -= new_commit_pos % arena->commit_size;
+        new_commit_pos = MIN(new_commit_pos, arena->reserve_size);
+
+        u8* mem = (u8*)arena + arena->commit_pos;
+        u64 commit_size = new_commit_pos - arena->commit_pos;
+
+        if (!plat_mem_commit(mem, commit_size)) {
+            return NULL;
+        }
+
+        arena->commit_pos = new_commit_pos;
+    }
+
+    arena->pos = new_pos;
+
+    u8* out = (u8*)arena + pos_aligned;
+
+    if (!non_zero) {
+        memset(out, 0, size);
+    }
+
+    return out;
+}
+
+void arena_pop(mem_arena* arena, u64 size) {
+    size = MIN(size, arena->pos - ARENA_BASE_POS);
+    arena->pos -= size;
+}
+
+void arena_pop_to(mem_arena* arena, u64 pos) {
+    u64 size = pos < arena->pos ? arena->pos - pos : 0;
+    arena_pop(arena, size);
+}
+
+void arena_clear(mem_arena* arena) {
+    arena_pop_to(arena, ARENA_BASE_POS);
+}
+
+mem_arena_temp arena_temp_begin(mem_arena* arena) {
+    return (mem_arena_temp) {
+        .arena = arena,
+        .start_pos = arena->pos
+    };
+}
+
+void arena_temp_end(mem_arena_temp temp) {
+    arena_pop_to(temp.arena, temp.start_pos);
+}
+
+static __thread mem_arena* _scratch_arenas[2] = { NULL, NULL };
+
+mem_arena_temp arena_scratch_get(mem_arena** conflicts, u32 num_conflicts) {
+    i32 scratch_index = -1;
+
+    for (i32 i = 0; i < 2; i++) {
+        b32 conflict_found = false;
+
+        for (u32 j = 0; j < num_conflicts; j++) {
+            if (_scratch_arenas[i] == conflicts[j]) {
+                conflict_found = true;
+                break;
+            }
+        }
+
+        if (!conflict_found) {
+            scratch_index = i;
+            break;
+        }
+    }
+
+    if (scratch_index == -1) {
+        return (mem_arena_temp){ 0 };
+    }
+
+    mem_arena** selected = &_scratch_arenas[scratch_index];
+
+    if (*selected == NULL) {
+        *selected = arena_create(MiB(64), MiB(1));
+    }
+
+    return arena_temp_begin(*selected);
+}
+
+void arena_scratch_release(mem_arena_temp scratch) {
+    arena_temp_end(scratch);
+}
+
+#if defined(_WIN32)
+
+#include <windows.h>
+
+u32 plat_get_pagesize(void) {
+    SYSTEM_INFO sysinfo = { 0 };
+    GetSystemInfo(&sysinfo);
+
+    return sysinfo.dwPageSize;
+}
+
+void* plat_mem_reserve(u64 size) {
+    return VirtualAlloc(NULL, size, MEM_RESERVE, PAGE_READWRITE);
+}
+
+b32 plat_mem_commit(void* ptr, u64 size) {
+    void* ret = VirtualAlloc(ptr, size, MEM_COMMIT, PAGE_READWRITE);
+    return ret != NULL;
+}
+
+b32 plat_mem_decommit(void* ptr, u64 size) {
+    return VirtualFree(ptr, size, MEM_DECOMMIT);
+}
+
+b32 plat_mem_release(void* ptr, u64 size) {
+    return VirtualFree(ptr, size, MEM_RELEASE);
+}
+
+
+#elif defined(__linux__)
+
+#ifndef _DEFAULT_SOURCE
+#define _DEFAULT_SOURCE
+#endif
+
+#include <unistd.h>
+#include <sys/mman.h>
+
+u32 plat_get_pagesize(void) {
+    return (u32)sysconf(_SC_PAGESIZE);
+}
+
+void* plat_mem_reserve(u64 size) {
+    void* out = mmap(NULL, size, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (out == MAP_FAILED) {
+        return NULL;
+    }
+    return out;
+}
+
+b32 plat_mem_commit(void* ptr, u64 size) {
+    i32 ret = mprotect(ptr, size, PROT_READ | PROT_WRITE);
+    return ret == 0;
+}
+
+b32 plat_mem_decommit(void* ptr, u64 size) {
+    i32 ret = mprotect(ptr, size, PROT_NONE);
+    if (ret != 0) return false;
+    ret = madvise(ptr, size, MADV_DONTNEED);
+    return ret == 0;
+}
+
+b32 plat_mem_release(void* ptr, u64 size) {
+    i32 ret = munmap(ptr, size);
+    return ret == 0;
+}
+
+
+#elif defined(__APPLE__)
+
+#include <unistd.h>
+#include <sys/mman.h>
+
+u32 plat_get_pagesize(void) {
+    return (u32)sysconf(_SC_PAGESIZE);
+}
+
+void* plat_mem_reserve(u64 size) {
+    void* out = mmap(NULL, size, PROT_NONE, MAP_PRIVATE | MAP_ANON, -1, 0);
+    if (out == MAP_FAILED) return NULL;
+    return out;
+}
+
+b32 plat_mem_commit(void* ptr, u64 size) {
+    return mprotect(ptr, size, PROT_READ | PROT_WRITE) == 0;
+}
+
+b32 plat_mem_decommit(void* ptr, u64 size) {
+    // Optional: macOS doesn’t have MADV_DONTNEED same semantics everywhere,
+    // but MADV_FREE is commonly used.
+    if (mprotect(ptr, size, PROT_NONE) != 0) return false;
+    return madvise(ptr, size, MADV_FREE) == 0;
+}
+
+b32 plat_mem_release(void* ptr, u64 size) {
+    return munmap(ptr, size) == 0;
+}
+#endif
